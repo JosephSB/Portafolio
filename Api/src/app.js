bf53fcfe532756a158dbfd5c7a93d4837a2e0340
JosephSB <jguku2002@gmail.com>
2022-01-31 14:56:52 -0500

    agregando directorio publico

--- Api/src/app.js
+++ Api/src/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const MainRouter = require("./router/MainRouter");
 
 const app = express();
+const DirectorioPublico = express.static(path.join(__dirname + "/public"));
 app.set("port", process.env.PORT || 4000);
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(DirectorioPublico);
 
 app.use("/", MainRouter);
 
